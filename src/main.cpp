b119c4bba3280008426b9fe5ed56b2ebe3ed6023
EgorKostuck <e.kostuck@iotans.by>
2021-12-29 13:51:54 +0300

    final

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 
 #include "mbed.h"
 
-#define WAIT_TIME_MS 500ms
+#define WAIT_TIME_MS 1000ms
 DigitalOut ledInfo(P0_5);
 DigitalOut ledLock(P0_6);
 DigitalOut ledUnlock(P0_7);
@@ -15,13 +15,17 @@ int main()
 
     while (true)
     {   
-        ledInfo.write(0);
-        ledLock.write(0);
-        ledUnlock.write(0);
+      ledUnlock = !ledUnlock;
+      ledLock = !ledLock;
+      ledInfo = !ledInfo;
+      ThisThread::sleep_for(WAIT_TIME_MS);
+        // ledInfo.write(0);
+        // ledLock.write(0);
+        // ledUnlock.write(0);
 
         
-        ThisThread::sleep_for(WAIT_TIME_MS);
-        ledInfo.write(1);
-        ThisThread::sleep_for(WAIT_TIME_MS);
+        // ThisThread::sleep_for(WAIT_TIME_MS);
+        // ledInfo.write(1);
+        // ThisThread::sleep_for(WAIT_TIME_MS);
     }
 }
